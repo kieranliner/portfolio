c8596de1b6c03396003a0f849795a98a40d8f237
kieran liner <keiranliner@gmail.com>
2024-03-23 13:32:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log("hey")
+// $(function(){
+//     console.log("hello")
+//     var str = '#len'; //increment by 1 up to 1-nelemnts
+//     $(document).ready(function(){
+//       var i, stop;
+//       i = 1;
+//       stop = 4; //num elements
+//       setInterval(function(){
+//         if (i > stop){
+//           return;
+//         }
+//         $('#len'+(i++)).toggleClass('bounce');
+//       }, 500)
+//     });
+//   });
+console.log("hey");
+ScrollReveal().reveal(".navbar-brand");
